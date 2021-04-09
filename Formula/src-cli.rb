4c59615a3a6139d708061239a25c8b05088a2894
goreleaserbot <goreleaser@carlosbecker.com>
2021-04-09 05:52:31 -0700

    Brew formula update for src-cli version 3.26.3

--- Formula/src-cli.rb
+++ Formula/src-cli.rb
@@ -5,16 +5,16 @@
 class SrcCli < Formula
   desc "Sourcegraph CLI"
   homepage "https://sourcegraph.com/"
-  version "3.26.2"
+  version "3.26.3"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sourcegraph/src-cli/releases/download/3.26.2/src-cli_3.26.2_darwin_amd64.tar.gz"
-    sha256 "f63f043e047a664ac63ae8cfd67098badeb90db600d9fe5086331ff987a9a4af"
+    url "https://github.com/sourcegraph/src-cli/releases/download/3.26.3/src-cli_3.26.3_darwin_amd64.tar.gz"
+    sha256 "0324cddb2fc3ddcf2bf9f06d3ee1aa4495367c869054bb40af5ff05916921eb6"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sourcegraph/src-cli/releases/download/3.26.2/src-cli_3.26.2_linux_amd64.tar.gz"
-    sha256 "65c79c0897cefa2d260f38f55270f94ceb8289baffe3f37d2d17611fa89683ef"
+    url "https://github.com/sourcegraph/src-cli/releases/download/3.26.3/src-cli_3.26.3_linux_amd64.tar.gz"
+    sha256 "2ad1f5845bafaea0807fd8896d0f41ba269d40ec43d9ae1b28a89fd7384045b5"
   end
 
   def install
