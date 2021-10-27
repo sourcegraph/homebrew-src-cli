77e0c522237f1c766ced807d535ff73e2d4aa726
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-27 11:19:33 +0200

    Brew formula update for src-cli version 3.33.3

--- Formula/src-cli.rb
+++ Formula/src-cli.rb
@@ -5,28 +5,28 @@
 class SrcCli < Formula
   desc "Sourcegraph CLI"
   homepage "https://sourcegraph.com/"
-  version "3.33.2"
+  version "3.33.3"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.2/src-cli_3.33.2_darwin_arm64.tar.gz"
-      sha256 "8c9912fa72a10d474105c5a988372d472494a62d3fb9bc8ace22fd010296967b"
+      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.3/src-cli_3.33.3_darwin_arm64.tar.gz"
+      sha256 "0d182c064b9410a59af8e8c7b1ea4a08f5f3865a0cd007d5f70598d3db3fc41d"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.2/src-cli_3.33.2_darwin_amd64.tar.gz"
-      sha256 "d6308b012a234c2e90d502eb3a3c847e3392d16bb760e429ffba2e79fb65b6f5"
+      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.3/src-cli_3.33.3_darwin_amd64.tar.gz"
+      sha256 "34649a9f4471bc3bf8e844a0346c0761210d7fb55acaea626254059cd5c89376"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.2/src-cli_3.33.2_linux_arm64.tar.gz"
-      sha256 "744f6dde13f04659360fb4742bbe9d72829f09ded7b2f4a808bf139186585ffb"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.2/src-cli_3.33.2_linux_amd64.tar.gz"
-      sha256 "807fc379d2052f2ec1ec7c76c477cc173f838c21d7c8f163f5a3676ca9a41b8b"
+      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.3/src-cli_3.33.3_linux_amd64.tar.gz"
+      sha256 "04504961703f06a5b99801d3a98605962c7eb2179e7c552f09c9960ed2ed7a8c"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/sourcegraph/src-cli/releases/download/3.33.3/src-cli_3.33.3_linux_arm64.tar.gz"
+      sha256 "e14b83c511e825c84635853d55fb133a32c4a23484173db902dd35c7f6b4ea24"
     end
   end
 
